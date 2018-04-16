23ba17b33e42a630faf2be3f9de54070c67d5309
AJ Tuquero <ajtuquero@gmail.com>
2018-04-15 23:51:40 -0400

    Add initial cloud models and migrations

--- event_planner_api/settings.py
+++ event_planner_api/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'tokenapi',
+    'cloud',
 ]
 
 # Authentication
